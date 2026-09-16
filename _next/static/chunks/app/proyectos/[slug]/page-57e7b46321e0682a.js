d83e62ba5701cff289cebd368f9d255cfa1f44e4
jcano-2024386 <jcano-2024386@kinal.edu.gt>
2026-09-16 11:08:49 -0600

    Deploy portfolio static site

---
+++ _next/static/chunks/app/proyectos/[slug]/page-57e7b46321e0682a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[510],{9617:function(e,n,s){Promise.resolve().then(s.bind(s,5466)),Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,649,971,117,744],function(){return e(e.s=9617)}),_N_E=e.O()}]);
